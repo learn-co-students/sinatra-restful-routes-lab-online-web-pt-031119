021d6cd9f15ef991291240524dcb0500b2af4dc8
MKanko <mkanko66@gmail.com>
2019-06-25 09:13:01 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # enable :sessions
+    # set :session_secret, "password_security"
+    
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index 
+  end 
+
+  get '/recipe/new' do
+    
+  end 
 
 end
