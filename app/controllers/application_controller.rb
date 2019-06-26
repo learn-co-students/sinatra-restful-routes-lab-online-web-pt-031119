b14fdd19d642a0eb38610da534653efca6d719fe
dsstewa <davidstewart@gmail.com>
2019-06-25 21:20:41 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,56 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+
+ # code actions here!
+
+get '/recipes' do 
+  
+  @recipes = Recipe.all
+  erb :index
+  end
+
+
+
+get '/recipes/new' do 
+erb :new
+end
+
+get '/recipes/:id/edit' do
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :edit
+end
+
+post '/recipes/new' do
+
+@recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+@recipe.save
+
+redirect to "/recipes/#{@recipe.id}"
+
+end
+
+patch '/recipes/:id' do 
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.name = params[:name]
+  @recipe.ingredients = params[:ingredients]
+  @recipe.save
+  redirect to '/recipes/:id'
+end
+
+  get '/recipes/:id' do
+  
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :show
+
+end
+
+
+
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+
+
+
+
+Recipe.create(:name => "Bomb.com Mac and Cheese", :ingredients => "milk, butter, cheese, elbow pasta", :cook_time => "20 minutes")
+Recipe.create(:name => "waldorf salad", :ingredients => "apples, cabbage, oil, vinegar", :cook_time => "0")
